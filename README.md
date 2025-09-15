4db06abaf214344d21485c46f280ff9765fcd31d
Suraj Shende <suraj.shende@thecadre.in>
2025-09-15 20:36:20 +0530

    Inital Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tiny Blog 📝
+
+This is a hobby project to understand how blog applications work and their architecture.
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    if (conn) {
+      console.log("MongoDB connected");
+    }
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Server is up and running...",
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
