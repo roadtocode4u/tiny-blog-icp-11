95fdeb62ae68e674426c7db4696ff3f60518c3c0
Suraj Shende <surajshende247@gmail.com>
2025-09-18 20:17:42 +0530

    feat: auth APIs

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import { postLogin, postSignup } from "./controllers/user.js";
 dotenv.config();
 
 const app = express();
@@ -26,6 +27,9 @@ app.get("/", (req, res) => {
     message: "Server is up and running...",
   });
 });
+
+app.post("/signup", postSignup);
+app.post("/login", postLogin);
 
 const PORT = process.env.PORT || 8080;
 
